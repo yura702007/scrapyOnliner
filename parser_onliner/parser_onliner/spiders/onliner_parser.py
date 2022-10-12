597fcc451236739c22d7e4b1702ef35de445d150
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-10-12 19:13:40 +0300

    add parse_link method

--- parser_onliner/parser_onliner/spiders/onliner_parser.py
+++ parser_onliner/parser_onliner/spiders/onliner_parser.py
@@ -11,7 +11,16 @@ class OnlinerSpider(scrapy.Spider):
         for category in categories:
             try:
                 if int(category.attrib['data-id']) in categories_number:
-                    links = category.css('a.catalog-navigation-list__dropdown-item::attr(href)').getall()
-                    print(links, '\n')
+                    catalog_list = category.css('div.catalog-navigation-list__aside-item')
+                    for item in catalog_list:
+                        title = item.css('div.catalog-navigation-list__aside-title::text').get()
+                        links = item.css('a.catalog-navigation-list__dropdown-item::attr(href)').getall()
+                        yield from response.follow_all(links, callback=self.parse_link)
+                        break
+                    break
             except ValueError:
                 pass
+
+    def parse_link(self, response):
+        div = response.css('div.schema-product')
+        print(div, '\n')
